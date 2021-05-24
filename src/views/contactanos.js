5f5eeba0b385f1f85862fc8fc6bb8651d8c58eda
Leandro Marrero <leandro.marrero@outlook.com>
2021-05-23 22:34:27 -0300

    view contactanos funcional, falta la alerta y enviar el mensaje

--- src/views/contactanos.js
+++ src/views/contactanos.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import phone from "../img/phone.svg";
-import email from "../img/Icon material-email.svg";
+import imgEmail from "../img/Icon material-email.svg";
 
 export default function Contactanos() {
   const [tamanoTitulo, setTamanoTitulo] = useState('');
@@ -10,6 +10,15 @@ export default function Contactanos() {
   const [tamanioImg, setTamanioImg] = useState("");
   const [margen, setMargen] = useState("");
   const [margenLeft, setMargenLeft] = useState("");
+
+  const [nombre, setNombre] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [email, setEmail] = useState("");
+  const [mensaje, setMensaje] = useState("");
+
+  const [fondoNombre, setFondoNombre] = useState("");
+  const [fondoTelefono, setFondoTelefono] = useState("");
+  const [fondoMensaje, setFondoMensaje] = useState("");
 
   useEffect(() => {
     if(window.screen.width < 780){
@@ -48,6 +57,42 @@ export default function Contactanos() {
     fontSize: tamanoSubTexto
   }
 
+  const cambiarNombre = (e) => {
+    setNombre(e.target.value);
+  }
+  const cambiarTelefono = (e) => {
+    setTelefono(e.target.value);
+  }
+  const cambiarEmail = (e) => {
+    setEmail(e.target.value);
+  }
+  const cambiarMensaje = (e) => {
+    setMensaje(e.target.value);
+  }
+
+  const enviar = () => {
+    let todoOk = true;
+    if(nombre === ''){
+        setFondoNombre("bg-warning");
+        todoOk = false;
+    }
+    if(telefono === ''){
+        setFondoTelefono("bg-warning");
+        todoOk = false;
+    } 
+    if(mensaje === ''){
+        setFondoMensaje('bg-warning');
+        todoOk = false;
+    }
+    if(todoOk){
+        //envio
+    }
+    else{
+        alert("Ups, nos faltan algunos datos para poder enviar su mensaje");
+    }
+
+  }
+
   return (
     <div>
         <div style={{ backgroundColor: "#FCAAAA"}} className="py-3 m-0">
@@ -55,13 +100,13 @@ export default function Contactanos() {
         </div>
         <div className="row mt-3 m-0">
             <div style={{ backgroundColor: "#C2C2C2"}} className={"shadow rounded col-12 mb-3 col-lg-5 m-0 d-flex justify-content-center "+ margenLeft}>
-                <form className="col-12 d-flex flex-column align-items-center">
+                <form id="form" className="col-12 d-flex flex-column align-items-center">
                     <span style={styleText} className="my-2">Completa con tus datos</span>
-                    <input className="form-control-lg form-control text-center" type="text" placeholder="Nombre y apellido" />
-                    <input className="form-control-lg form-control text-center my-2" type="text" placeholder="Teléfono" />
-                    <input className="form-control-lg form-control text-center" type="text" placeholder="Email" />
-                    <textarea className="form-control mt-2" rows="4" placeholder="Ingresa tu pregunta"></textarea>
-                    <button type="submit" className="mt-3 btn btn-primary mb-2" >Enviar mensaje</button>
+                    <input className={"form-control-lg form-control text-center " + fondoNombre} type="text" placeholder="Nombre y apellido" value={nombre} onChange={cambiarNombre}/>
+                    <input className={"form-control-lg form-control text-center my-2 "+ fondoTelefono} type="text" placeholder="Teléfono" value={telefono} onChange={cambiarTelefono}/>
+                    <input className="form-control-lg form-control text-center " type="text" placeholder="Email" value={email} onChange={cambiarEmail}/>
+                    <textarea className={"form-control mt-2 " + fondoMensaje} rows="4" placeholder="Ingresa tu pregunta" value={mensaje} onChange={cambiarMensaje} />
+                    <button type="button" onClick={enviar} className="mt-3 btn btn-primary mb-2" >Enviar mensaje</button>
                 </form>
             </div>
             <div style={{ backgroundColor: "#FCAAAA"}} className={"shadow rounded col-12 col-lg-6 d-flex flex-column align-items-center "+ margenLeft}>
@@ -72,11 +117,11 @@ export default function Contactanos() {
                         <span style={styleSubText}>098361013</span>
                     </div>
                     <div className="d-flex justify-content-center align-items-center my-2">
-                        <img src={email} style={{width: tamanioImg}} className="mr-2 "/>
+                        <img src={imgEmail} style={{width: tamanioImg}} className="mr-2 "/>
                         <span style={styleSubText}>leandro.marrero@outlook.com</span>
                     </div>
                     <div style={{margin: margen}}>
-                        <a href="#"><button className="mt-3 btn btn-lg btn-success mb-2" >Ir directo a WhatsApp</button></a>
+                        <a target="_blank"href="https://api.whatsapp.com/send?phone=59898361013"><button className="mt-3 btn btn-lg btn-success mb-2" >Ir directo a WhatsApp</button></a>
                     </div>
                 </div>
             </div>
