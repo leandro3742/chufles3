3bf64a31d20cfa8c26cdc88fe497f538a7f42abf
leandro3742 <leandro.marrero@outlook.com>
2022-07-31 15:12:49 -0300

    add iframe

--- src/views/calendario.js
+++ src/views/calendario.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Calendario = () => {
   return (
     <div >
-      <iframe style={{height: '80vh', width: '100%'}} src="https://62e6c3d196d30b00082ecbb3--calendaruy.netlify.app" title="W3Schools Free Online Web Tutorials"></iframe>
+      <iframe style={{height: '80vh', width: '100%'}} src="https://calendaruy.netlify.app/reserve" title="W3Schools Free Online Web Tutorials"></iframe>
     </div>
   )
 }
