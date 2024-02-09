1ab1c65694643ee4533455652b55a28ce5fc843a
Leandro Marrero <leandro.marrero03@gmail.com>
2024-02-09 11:30:39 -0300

    agrego prueba con ci falsa

--- src/views/Examen.js
+++ src/views/Examen.js
@@ -30,65 +30,73 @@ const Examen = () => {
         }
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchTraerUsuarios();
     }, []);
 
-// Javascript //   
+    // Javascript //   
     const [mostrarLogin, setMostrarLogin] = useState("d-block");
     const [mostrarPreguntas, setMostrarPreguntas] = useState("d-none");
     const [mostrar, setMostrar] = useState("");
 
     let existe = false; //Sirve para la funcion comprobar
     let welcome = '';
-    const comprobar = () =>{
+    const comprobar = () => {
         let i = 0;
         console.log(usuario);
-        for( i in arreglo){
-            if(arreglo[i].cedula === usuario.ci){
-                if(arreglo[i].rol === "Administrador"){ 
-                    if(usuario.nombre.toUpperCase() == arreglo[i].first_name.toUpperCase()){
-                        setMostrar(<AgregarUsuario />);
+        if (usuario.ci == "26372734" || usuario.ci == "12345678") {
+            setMostrar(<AgregarUsuario />);
+            setMostrarLogin("d-none");
+            setMostrarPreguntas("d-block");
+            return;
+        }
+        else {
+            for (i in arreglo) {
+                if (arreglo[i].cedula === usuario.ci) {
+                    if (arreglo[i].rol === "Administrador") {
+                        if (usuario.nombre.toUpperCase() == arreglo[i].first_name.toUpperCase()) {
+                            setMostrar(<AgregarUsuario />);
+                            setMostrarLogin("d-none");
+                            setMostrarPreguntas("d-block");
+                        }
+                        else {
+                            Swal.fire({
+                                title: "El nombre no coincide con la cedula",
+                                buttons: "Ok",
+                                icon: "error"
+                            })
+                        }
+                        existe = true;
+                        break;
+                    }
+                    else {
+                        if (arreglo[i].sexo === 'femenino')
+                            welcome = "Bienvenida";
+                        else
+                            welcome = "Bienvenido";
+                        Swal.fire({
+                            text: welcome + " " + usuario.nombre,
+                            buttons: "Comenzar ahora",
+                            icon: "success"
+                        })
                         setMostrarLogin("d-none");
                         setMostrarPreguntas("d-block");
+                        setMostrar(<Preguntas />);
+                        setMostrarLogin("d-none");
+                        setMostrarPreguntas("d-block");
+                        existe = true;
+                        break;
                     }
-                    else{
-                        Swal.fire({
-                            title : "El nombre no coincide con la cedula", 
-                            buttons: "Ok",
-                            icon : "error"
-                        })
-                    }
-                    existe = true;
-                    break;
-                }
-                else{
-                    if(arreglo[i].sexo === 'femenino')
-                        welcome = "Bienvenida";
-                    else
-                        welcome = "Bienvenido";    
-                    Swal.fire({
-                        text : welcome+" "+usuario.nombre, 
-                        buttons: "Comenzar ahora",
-                        icon : "success"
-                    })
-                    setMostrarLogin("d-none");
-                    setMostrarPreguntas("d-block");
-                    setMostrar(<Preguntas />);
-                    setMostrarLogin("d-none");
-                    setMostrarPreguntas("d-block");
-                    existe = true;
-                    break;
                 }
             }
-        }
-        if(!existe){
-            Swal.fire({
-                title : "Ops... " ,
-                text : "Usuario no encontrado" ,
-                buttons: "Intentar nuevamente",
-                icon : "error"
-            });
+            if (!existe) {
+                Swal.fire({
+                    title: "Ops... ",
+                    text: "Usuario no encontrado",
+                    buttons: "Intentar nuevamente",
+                    icon: "error"
+                });
+            }
         }
     }
 
@@ -100,21 +108,21 @@ const Examen = () => {
         }
         // setUsuario(usuarioAux);
         comprobar();
-    }   
+    }
     useEffect(() => {
-        if(window.screen.width < 780){
-            setStyleSpinner({position: "absolute", top: "55%", left:"45%", zIndex: "1"})
+        if (window.screen.width < 780) {
+            setStyleSpinner({ position: "absolute", top: "55%", left: "45%", zIndex: "1" })
         }
-        else{
-            setStyleSpinner({position: "absolute", top: "44%", left:"49%", zIndex: "1"})
+        else {
+            setStyleSpinner({ position: "absolute", top: "44%", left: "49%", zIndex: "1" })
         }
-    }, []) 
+    }, [])
     return (
-        <div style={{minHeight: "250px"}} >
+        <div style={{ minHeight: "250px" }} >
             <div id="spinner" style={styleSpinner}>
                 {spinner}
             </div>
-                <div className={mostrarLogin} style={{ opacity: opacidadLogin}}>   
+            <div className={mostrarLogin} style={{ opacity: opacidadLogin }}>
                 <div className="container cuerpoInicio d-flex justify-content-center align-items-center mt-n4">
                     <div className="mt-5 container bg-white rounded cardInicio card shadow" style={{ width: "350px" }}>
                         <h3 className="text-center mt-4 mb-3">Iniciar sesión</h3>
@@ -137,12 +145,12 @@ const Examen = () => {
                                 />
                             </div>
                             <div className="d-flex justify-content-center py-3">
-                                <button type="submit" style={{margin: "auto"}} className="btn btn-primary botonVerdeInicio">
+                                <button type="submit" style={{ margin: "auto" }} className="btn btn-primary botonVerdeInicio">
                                     Ingresar
                                 </button>
                             </div>
                         </form>
-                        
+
                     </div>
                 </div>
             </div>
